f6b9cbb1ecd0a0ad5aaba49b2509c99b82b9904a
Clifton Griffin <clifgriffin@gmail.com>
2016-02-23 11:33:44 -0500

    Auto unrequire postal code when selected billing or shipping country does not require a postal code.

--- core/library/Lookup.php
+++ core/library/Lookup.php
@@ -247,6 +247,14 @@ class ShoppLookup {
 		return (array)apply_filters('shopp_country_gst', $_);
 	}
 
+	public static function country_no_postal_codes() {
+		$_ = array(
+			'AO','AG','AW','BS','BZ','BJ','BW','BF','BI','CM','CF','KM','CG','CD','CK','CI','DJ','DM','GQ','ER','FJ','TF','GM','GH','GD','GN','GY','HK','IE','JM','KE','KI','MO','MW','ML','MR','MU','MS','NR','AN','NU','KP','PA','QA','RW','KN','LC','ST','SA','SC','SL','SB','SO','ZA','SR','SY','TZ','TL','TK','TO','TT','TV','UG','AE','VU','YE','ZW'
+		);
+
+		return (array)apply_filters('shopp_country_no_postal_codes', $_);
+	}
+
 	public static function postcodes ( array $countries = array() ) {
 
 		$postcodes = array();
