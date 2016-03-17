e896ce485316b2ff4d91a67dedfdca32d3342a19
Clifton Griffin <clifgriffin@gmail.com>
2016-03-17 10:13:56 -0400

    Setup initial gulp scripts task and watch.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
 var gulp = require('gulp');
+var rename = require('gulp-rename');
+var notify = require('gulp-notify');
+var uglify = require('gulp-uglify');
+var gutil = require('gulp-util');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('scripts', function() {
+  return gulp.src(['core/ui/**/*.js', '!core/ui/**/*.min.js'])
+  .pipe(uglify().on('error', gutil.log))
+  .pipe(rename({ extname: '.min.js' }))
+  .pipe(gulp.dest('core/ui'))
+  .pipe(notify({ message: 'Scripts task complete' }));
 });
+
+// Watch
+gulp.task('watch', function() {
+    gulp.watch('core/ui/**/!(*.min)*.js', ['scripts']);
+});
+
+// Default task
+gulp.task('default', ['scripts', 'watch']);
