773685aba04863a472a6729ade8ebe749b4597f4
Jon Davis <jond@ingenesis.net>
2017-05-26 10:29:59 -0700

    Fixed #3775 Added exclude field to categories widget
    The remaining changes were already implemented.

--- core/ui/widgets/categories.php
+++ core/ui/widgets/categories.php
@@ -41,7 +41,8 @@ class ShoppCategoriesWidget extends WP_Widget {
 			'products' => 'off',
 			'hierarchy' => 'off',
 			'depth' => '0',
-			'showsmart' => 'hide'
+			'showsmart' => 'hide',
+			'exclude'	=> ''
 		), $options);
 		?>
 		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title'); ?></label>
@@ -52,6 +53,11 @@ class ShoppCategoriesWidget extends WP_Widget {
 		<input type="hidden" name="<?php echo $this->get_field_name('products'); ?>" value="off" /><input type="checkbox" id="<?php echo $this->get_field_id('products'); ?>" name="<?php echo $this->get_field_name('products'); ?>" value="on"<?php echo $options['products'] == "on"?' checked="checked"':''; ?> /><label for="<?php echo $this->get_field_id('products'); ?>"> <?php Shopp::_e('Show product counts'); ?></label><br />
 		<input type="hidden" name="<?php echo $this->get_field_name('hierarchy'); ?>" value="off" /><input type="checkbox" id="<?php echo $this->get_field_id('hierarchy'); ?>" name="<?php echo $this->get_field_name('hierarchy'); ?>" value="on"<?php echo $options['hierarchy'] == "on"?' checked="checked"':''; ?> /><label for="<?php echo $this->get_field_id('hierarchy'); ?>"> <?php Shopp::_e('Show hierarchy'); ?></label><br />
 		<input type="hidden" name="<?php echo $this->get_field_name('depth'); ?>" value="0" /><input type="checkbox" id="<?php echo $this->get_field_id('depth'); ?>" name="<?php echo $this->get_field_name('depth'); ?>" value="1"<?php echo $options['depth'] == "1"?' checked="checked"':''; ?> /><label for="<?php echo $this->get_field_id('depth'); ?>"> <?php Shopp::_e('Hide child categories'); ?></label><br />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('exclude'); ?>"><?php _e( 'Exclude' ); ?></label> <input type="text" name="<?php echo $this->get_field_name('exclude'); ?>" id="<?php echo $this->get_field_id('exclude'); ?>" class="widefat" value="<?php echo $options['exclude']; ?>" />
+			<br />
+			<small><?php Shopp::_e( 'Category ID\'s, separated by commas.' ); ?></small>
 		</p>
 		<p><label for="<?php echo $this->get_field_id('showsmart'); ?>"><?php Shopp::_e('Smart Categories:'); ?>
 			<select id="<?php echo $this->get_field_id('showsmart'); ?>" name="<?php echo $this->get_field_name('showsmart'); ?>" class="widefat"><option value="false"><?php _e('Hide'); ?></option><option value="before"<?php echo $options['showsmart'] == "before"?' selected="selected"':''; ?>><?php Shopp::_e('Include before custom categories'); ?></option><option value="after"<?php echo $options['showsmart'] == "after"?' selected="selected"':''; ?>><?php Shopp::_e('Include after custom categories'); ?></option></select></label></p>
